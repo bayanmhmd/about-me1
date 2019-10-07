8d3d7cca1b9d1d711b172b696ff470b163c08d3f
bayan <bayana.mhmd99@gmail.com>
2019-10-07 16:02:51 +0300

    kk

---
+++ about-me.js
@@ -0,0 +1,50 @@
+var name = prompt('what is your name?');
+           alert('Welcome    '    +      name);
+        
+var answer = prompt('Do i love mansaf?');
+      if(answer == 'yes'){
+          alert('I am sorry , i dont like it ');
+      }    
+      else if(answer == 'no'){
+          alert('You right');
+      }       else{
+          alert(' i love healthy food');
+      }
+var ans = prompt('Do i read books?');
+if(ans == 'yes'){
+    alert('Great , i do');
+    
+}    
+else if(ans == 'no'){
+    alert('I like to keep have a knowladge ');
+}       else{
+    alert('keep reading like me');
+    }
+ var answ = prompt('Do i love sports and walking?');
+ if(answ == 'yes'){
+    alert('Yes, but  i am fat *_* , any way my favourite its a basketball ');
+}    
+else if(answ == 'no'){
+    alert('oooo i dont want to  be fat  - __ -');
+}       else{
+    alert('i like walk');
+
+}
+var an= prompt('Do i like romantic movies?')
+if(an == 'yes'){
+    alert('i am sorry , i dont like it ');
+}    
+else if(an == 'no'){
+    alert('You right, i like horror and action movies');
+}       else{
+    alert('i like horror and action movies');
+}
+var a =prompt('Do you think i want to live in paris?');
+if(a == 'yes'){
+    alert('i wish but no money ');
+}    
+else if(a == 'no'){
+    alert('You right');
+}       else{
+    alert(' i would like to live in USA');
+}
